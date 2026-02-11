3e4206afa89c87ef9f6f7565a50c5cfee4e86f21
z10mx7 <z10mx7@protonmail.com>
2026-02-11 05:04:48 -0700

    add /monitor

--- main.go
+++ main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{
-		AppName:      getEnv("APP_NAME", "mithril-rev"),
+		AppName: getEnv("APP_NAME", "mithril-rev"),
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
 			code := fiber.StatusInternalServerError
 			if e, ok := err.(*fiber.Error); ok {
@@ -44,6 +46,11 @@ func main() {
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"status": "ok"})
 	})
+
+	app.Get("/monitor", monitor.New(monitor.Config{
+		Title:   getEnv("APP_NAME", "mithril-rev") + " Monitor",
+		Refresh: 3 * time.Second,
+	}))
 
 	port := getEnv("PORT", "4000")
 	log.Printf("Starting server on port %s", port)
