ded7973addfa966ffdaba0ce60161526b39c362c
z10mx7 <z10mx7@protonmail.com>
2026-02-13 17:41:16 -0700

    rm old

--- old/routes/console.go
+++
@@ -1,9 +0,0 @@
-package routes
-
-// SetupConsoleRoutes sets up console command routes
-// This file is for defining console commands that can be run via artisan
-func SetupConsoleRoutes() {
-	// Console commands are typically registered in main.go's runCLI() function
-	// This file is a placeholder for future console command organization
-}
-
